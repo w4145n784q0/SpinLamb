28367d0b4e829267887ec0554e68f9ec3456536e
直人 渡辺 <230080@jc-21.jp>
2024-12-09 18:29:10 +0900

    レイキャスト途中

--- Enemy.cpp
+++ Enemy.cpp
@@ -5,6 +5,8 @@
 #include"Engine/Camera.h"
 #include"Engine/SphereCollider.h"
 
+#include"Player.h"
+
 Enemy::Enemy(GameObject* parent)
 	:GameObject(parent,"Enemy"),hModel_Enemy(-1)
 {
@@ -18,16 +20,36 @@ void Enemy::Initialize()
 {
 	hModel_Enemy = Model::Load("enemy.fbx");
 	transform_.position_ = { 0,0,7 };
-	SphereCollider* col_body = new SphereCollider(XMFLOAT3(0, 0, 0), 0.1f);
-	this->AddCollider(col_body);
 
-	SphereCollider* col_eye = new SphereCollider(XMFLOAT3(0, 0, 0), 8.0f);
+	EnemyPosition = XMLoadFloat3(&this->transform_.position_);
+
+	SphereCollider* col_eye = new SphereCollider(XMFLOAT3(0, 0, 0), 5.0f);
 	this->AddCollider(col_eye);
 
 }
 
 void Enemy::Update()
 {
+	switch (enemy_state)
+	{
+	case Enemy::S_IDLE:
+		UpdateIdle();
+		break;
+	case Enemy::S_SUSPICIOUS:
+		break;
+	case Enemy::S_CHASE:
+		break;
+	case Enemy::S_ATTACK:
+		break;
+	case Enemy::S_MOVE:
+		break;
+	case Enemy::S_WINCE:
+		break;
+	case Enemy::S_MAX:
+		break;
+	default:
+		break;
+	}
 }
 
 void Enemy::Draw()
@@ -40,10 +62,33 @@ void Enemy::Release()
 {
 }
 
+void Enemy::UpdateIdle()
+{
+	Player pPlayer = (Player*)FindObject("Player");
+	//XMFLOAT3 PlayerPos = pPlayer.GetPosition();
+	//XMVECTOR v = XMLoadFloat3(&PlayerPos);
+	
+	
+	
+
+}
+
+void Enemy::UpdateChase()
+{
+
+}
+
 void Enemy::OnCollision(GameObject* pTarget)
 {
 	if (pTarget->GetObjectName() == "Player")
 	{
 		transform_.position_.y = 2.0f;
 	}
+}
+
+bool Enemy::ComparePosition(XMFLOAT3 pos)
+{
+	if(transform_.position_.x)
+
+	return false;
 }
--- Enemy.h
+++ Enemy.h
@@ -5,6 +5,20 @@ class Enemy :
 {
 private:
     int hModel_Enemy;
+
+	enum EnemyState {
+		S_IDLE = 0,//ë“ã@
+		S_SUSPICIOUS,//âˆÇµÇﬁ
+		S_CHASE,//í«Ç¢Ç©ÇØÇÈ
+		S_ATTACK,//çUåÇ
+		S_MOVE,//à⁄ìÆ
+		S_WINCE,//Ç–ÇÈÇﬁ
+		S_MAX
+	};
+	EnemyState enemy_state;
+
+	XMVECTOR EnemyPosition;
+
 public:
 	Enemy(GameObject* parent);
 	~Enemy();
@@ -13,7 +27,14 @@ public:
 	void Update() override;
 	void Draw() override;
 	void Release() override;
+	void UpdateIdle();
+	void UpdateSuspicious();
+	void UpdateChase();
+	void UpdateAttack();
+	void UpdateMove();
+	void UpdateWince();
 
 	void OnCollision(GameObject* pTarget) override;
+	bool ComparePosition(XMFLOAT3 pos);
 };
 
